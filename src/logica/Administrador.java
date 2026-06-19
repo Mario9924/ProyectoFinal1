33f8cce05cb766f719b1c4bcce91b8e43dfb5937
Mario Gutierrez <169930559+Mario9924@users.noreply.github.com>
2026-06-20 01:46:18 +0200

    Se crean las clases 'Administrador' y 'Normal' con el constructor de la madre por el momento

---
+++ src/logica/Administrador.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+/**
+ *
+ * @author Mario_
+ */
+public class Administrador extends Usuario {
+    
+    public Administrador(String emailIn, String passIn) {
+        super(emailIn, passIn);
+    }
+    
+    
+}
---
+++ src/logica/Normal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+/**
+ *
+ * @author Mario_
+ */
+public class Normal extends Usuario {
+    
+    public Normal(String emailIn, String passIn) {
+        super(emailIn, passIn);
+    }
+    
+}
