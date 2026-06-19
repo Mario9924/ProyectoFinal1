57836a1a82731b89a5511626044d9c1e570ff143
Mario Gutierrez <169930559+Mario9924@users.noreply.github.com>
2026-06-20 00:40:37 +0200

    Se añade a la logica de la aplicacion una función de registro de errores en LOG

--- src/logica/Log.java
+++ src/logica/Log.java
@@ -4,14 +4,38 @@
  */
 package logica;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.time.LocalDateTime; // Import the LocalDateTime class
+import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
+// https://www.w3schools.com/JAVA/java_date.asp
+
 /**
  *
  * @author Mario_
  */
 public class Log {
     
-    public static void escribirLog(String errorIn){
+    public static void escribirLog(String errorIn) {
+        // Obtenemos la fecha actual, con la hora también
+        LocalDateTime myDateObj = LocalDateTime.now();
+        // Damos formato a los datos anteriores
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formattedDate = myDateObj.format(myFormatObj);
+        File fichero = new File(".\\src\\ficheros", "log.txt");
+        try (
+                FileWriter fw = new FileWriter(fichero, Charset.forName("ISO-8859-1"), true)
+                )
+        {
+            fw.write(formattedDate + " " + errorIn + "\n");
+        } catch (IOException ex) {
+            //Si da error también a la hora de guardar el error también lo mostraremos
+            System.out.println("Ha ocurrido un error al escribir en el log de errores: " + ex);
+            escribirLog("" +ex);
+        }
         
     }
-    
+
 }
